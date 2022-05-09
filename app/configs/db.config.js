3ac39e6b9094ac0a10c422bf8939e94caa780550
gilangcsy <gilangchandra9@gmail.com>
2022-05-09 11:25:21 +0700

    configuration

--- app/configs/db.config.js
+++ app/configs/db.config.js
@@ -1,8 +1,25 @@
+// module.exports = {
+//     HOST: 'localhost',
+//     USER: 'postgres',
+//     PASSWORD: 'postgres',
+//     DB: 'hr_management',
+//     dialect: 'postgres',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     },
+//     timezone: "+07:00",
+//     VERSION: '/api/v1',
+//     SCHEMA: 'public'
+// }
+
 module.exports = {
-    HOST: 'localhost',
-    USER: 'postgres',
-    PASSWORD: 'postgres',
-    DB: 'hr_management',
+    HOST: '95.111.202.9',
+    USER: 'ids',
+    PASSWORD: '123456789',
+    DB: 'ids',
     dialect: 'postgres',
     pool: {
         max: 5,
