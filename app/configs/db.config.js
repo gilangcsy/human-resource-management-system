18cbca8267aff3e706bf7e8c8e907b4a81b9cfac
gilangcsy <gilangchandra9@gmail.com>
2022-05-20 13:18:16 +0700

    change db config

--- app/configs/db.config.js
+++ app/configs/db.config.js
@@ -1,25 +1,8 @@
-module.exports = {
-    HOST: 'localhost',
-    USER: 'postgres',
-    PASSWORD: 'postgres',
-    DB: 'hr_management',
-    dialect: 'postgres',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    timezone: "+07:00",
-    VERSION: '/api/v1',
-    SCHEMA: 'public'
-}
-
 // module.exports = {
-//     HOST: '95.111.202.9',
-//     USER: 'ids',
-//     PASSWORD: '123456789',
-//     DB: 'ids',
+//     HOST: 'localhost',
+//     USER: 'postgres',
+//     PASSWORD: 'postgres',
+//     DB: 'hr_management',
 //     dialect: 'postgres',
 //     pool: {
 //         max: 5,
@@ -31,3 +14,20 @@ module.exports = {
 //     VERSION: '/api/v1',
 //     SCHEMA: 'public'
 // }
+
+module.exports = {
+    HOST: '95.111.202.9',
+    USER: 'ids',
+    PASSWORD: '123456789',
+    DB: 'ids',
+    dialect: 'postgres',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    timezone: "+07:00",
+    VERSION: '/api/v1',
+    SCHEMA: 'public'
+}
